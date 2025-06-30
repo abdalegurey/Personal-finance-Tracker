e7a8b440aea63a47f95353ffaf33b9b361abf0fb
abdalegurey <[200~abdalegureyguled@gmail.com~>
2025-06-30 12:45:09 +0300

    changed Port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import {logger} from "./middlewares/logger.js"
 import { Notfound } from './middlewares/NotFound.js';
 import uploadRoutes from "./routes/upload.js"
 
-const PORT = process.env.PORT  || 3000;
+const PORT = process.env.PORT  || 5000;
 import {limiter} from "./middlewares/ratelimit.js"
 import { swaggerSpec } from './utils/Swagger.js';
 
